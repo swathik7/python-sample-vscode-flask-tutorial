6c4c55e1568c1a7ea6fd676a3d5e94837a1edff1
swathik7 <89963349+swathik7@users.noreply.github.com>
2021-11-22 17:45:30 +0530

    Update startup.py

--- startup.py
+++ startup.py
@@ -5,7 +5,7 @@ of this structure, a file like webapp.py cannot be run directly as the startup
 file through Gunicorn; the result is "Attempted relative import in non-package".
 
 The solution is to provide a simple alternate startup file, like this present
-startup.py, that just imports the app object. You can then just specify
+startup.py,  that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
 
